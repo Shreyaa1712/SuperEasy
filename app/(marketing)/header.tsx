8ff82276b7d8805605fd07b06ba834a30b6a92c2
Shreyaa <shreyaajalan17@gmail.com>
2024-08-18 22:22:13 +0530

    ..

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
 
 export const Header =() => {
@@ -6,8 +10,27 @@ export const Header =() => {
         <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
-        <h1 className="text-2xl font-extrabold text-green-600 tracking-wind"></h1>
+        <h1 className="text-2xl font-extrabold text-green-600 tracking-wind"> SpeakEasy</h1>
         </div>
+        <ClerkLoading> 
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+        </ClerkLoading>
+        <ClerkLoaded>
+            <SignedIn>
+                <UserButton
+                aterSignOutUrl="/"
+                />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton
+                mode="modal"
+                afterSignInUrl= "/learn"  
+                afterSignUpUrl= "/learn">        
+                <Button size="lg" variant="ghost">
+                    Log In </Button>   
+            </SignInButton>
+            </SignedOut>
+        </ClerkLoaded>
         </div>
         </header>
     );
